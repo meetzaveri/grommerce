6fbe7e5929b007b2338252a1fe16189b2db4f517
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-05 09:37:27 +0530

    Made requests to JSON server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,12 +8,14 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
+/*
 // Hardcoded data
 const customers = [
   { id: "1", name: "John Doe", email: "jdoe@gmail.com", age: 35 },
   { id: "2", name: "Steve Smith", email: "steve@gmail.com", age: 25 },
   { id: "3", name: "Sara Williams", email: "sara@gmail.com", age: 32 }
 ];
+*/
 
 const CustomerType = new GraphQLObjectType({
   name: "Customer",
@@ -32,18 +35,26 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLString }
       },
+
       resolve(parentValue, args) {
+        /*
         for (let i = 0; i < customers.length; i++) {
           if (customers[i].id == args.id) {
             return customers[i];
           }
         }
+        */
+        return axios
+          .get("http://localhost:3000/customers/" + args.id)
+          .then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        return axios
+          .get("http://localhost:3000/customers")
+          .then(res => res.data);
       }
     }
   }
