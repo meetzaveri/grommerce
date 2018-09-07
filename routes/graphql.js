038b01debeeb6d5e3c51d855622070bd64024fb2
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-07 10:23:47 +0530

    Added Schema in seperate Models Module

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
-const schema = require("../schema");
+const schema = require("../models/schema");
 const Router = express.Router();
 
 Router.get("/", (req, res, next) => {
