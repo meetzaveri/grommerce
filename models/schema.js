1b8c03bb97052a258cb7136e33ab7889f0e1f43f
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-08 09:18:29 +0530

    Added a new Schema for Authentication

--- models/schema.js
+++ models/schema.js
@@ -1,115 +1,29 @@
-const axios = require("axios");
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema,
-  GraphQLList,
-  GraphQLNonNull
-} = require("graphql");
+import { makeExecutableSchema } from "graphql-tools";
 
-/*
-// Hardcoded data
-const customers = [
-  { id: "1", name: "John Doe", email: "jdoe@gmail.com", age: 35 },
-  { id: "2", name: "Steve Smith", email: "steve@gmail.com", age: 25 },
-  { id: "3", name: "Sara Williams", email: "sara@gmail.com", age: 32 }
-];
-*/
+const TEMP_USER = {
+  _id: "1",
+  email: "reeversedev@gmail.com"
+};
 
-const CustomerType = new GraphQLObjectType({
-  name: "Customer",
-  fields: () => ({
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  })
-});
-
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    customer: {
-      type: CustomerType,
-      args: {
-        id: { type: GraphQLString }
-      },
-
-      resolve(parentValue, args) {
-        /*
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id == args.id) {
-            return customers[i];
-          }
-        }
-        */
-        return axios
-          .get("http://localhost:3000/customers/" + args.id)
-          .then(res => res.data);
-      }
-    },
-    customers: {
-      type: new GraphQLList(CustomerType),
-      resolve(parentValue, args) {
-        return axios
-          .get("http://localhost:3000/customers")
-          .then(res => res.data);
-      }
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      return "Hello World";
     }
   }
+};
+export const Schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
-
-//Mutations
-const mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    addCustomer: {
-      type: CustomerType,
-      args: {
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        email: { type: new GraphQLNonNull(GraphQLString) },
-        age: { type: new GraphQLNonNull(GraphQLInt) }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .post("http://localhost:3000/customers", {
-            name: args.name,
-            email: args.email,
-            age: args.age
-          })
-          .then(res => res.data);
-      }
-    },
-    deleteCustomer: {
-      type: CustomerType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .delete("http://localhost:3000/customers/" + args.id)
-          .then(res => res.data);
-      }
-    },
-    editCustomer: {
-      type: CustomerType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-        age: { type: GraphQLInt }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .patch("http://localhost:3000/customers/" + args.id, args)
-          .then(res => res.data);
-      }
-    }
-  }
-});
-
-module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation
-});
+export function context(headers, secrets) {
+  return {
+    headers,
+    secrets
+  };
+}
