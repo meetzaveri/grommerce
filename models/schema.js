6e7cceb07bca955fceb9ed9bbb08d83f8bc943bb
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-09 12:12:48 +0530

    Setup babel for server and fixed madeExecutableSchema is not a function issue

--- models/schema.js
+++ models/schema.js
@@ -1,4 +1,4 @@
-const makeExecutableSchema = require("graphql-tools/dist/makeExecutableSchema");
+const { makeExecutableSchema } = require("graphql-tools");
 
 const TEMP_USER = {
   _id: "1",
@@ -17,11 +17,10 @@ const resolvers = {
     }
   }
 };
-module.exports = ("Schema",
-makeExecutableSchema({
+const executableSchema = makeExecutableSchema({
   typeDefs,
   resolvers
-}));
+});
 module.exports = function context(headers, secrets) {
   return {
     headers,
