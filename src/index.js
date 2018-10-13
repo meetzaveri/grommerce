6b515c171fc28c40ab2ee2d59c1f24bed9b9d9c8
Prateek Gogia <gogia.prateek@hotmail.com>
2018-10-13 11:35:05 +0530

    Stable Parcel configuration to get going

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <h1>Hello {this.props.name}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+let App = document.getElementById("root");
+
+ReactDOM.render(<HelloMessage name="Yomi" />, App);
