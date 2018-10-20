abb52b1009d1b3a67ff1435d2aa03be0902547d6
meetzaveri <meethpatel96@gmail.com>
2018-10-20 13:23:50 +0530

    [FEATURE] : Auth and Error boundaries wrappers added

---
+++ src/wrappers/authWrapper.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Routes from "../config/routes";
+
+//Auth Wrapper as part of navigation guard
+export const auth = Component => {
+  class Auth extends React.Component {
+    state = {};
+    componentDidMount() {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.log("token not available");
+        this.props.history.replace(Routes.login);
+      }
+    }
+    render() {
+      // re-check the token
+      const token = localStorage.getItem("token");
+      return token ? <Component {...this.props} /> : null;
+    }
+  }
+
+  return Auth;
+};
