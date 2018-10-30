4489f2ad384112291b1c11d2e4525826d23b8595
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-10-31 00:04:26 +0530

    Antd removed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss';
-
-import { Layout, Menu } from 'antd';
-
-const { Header, Content, Footer } = Layout;
 import Login from './components/login';
 import Routes from './config/routes';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -12,35 +8,7 @@ class Main extends React.Component {
   render() {
     return (
       <Router>
-        <Layout>
-          <Header>
-            <div className="logo">Grommerce</div>
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px' }}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
-          </Header>
-          <Content style={{ padding: '0 50px' }}>
-            <div
-              style={{
-                background: '#fff',
-                padding: 24,
-                minHeight: 280,
-                display: 'flex',
-                justifyContent: 'center'
-              }}
-            >
-              <Route path={Routes.login} component={Login} />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Grommerce ©2018</Footer>
-        </Layout>
+        <Route path={Routes.login} component={Login} />
       </Router>
     );
   }
