547f5e17e8b59e73844ea607da3434dcf0e18398
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-10-31 01:15:54 +0530

    New Login Page

--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Errorboundary from '../wrappers/errorBoundary';
-
-class NormalLoginForm extends React.Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
+// import PropTypes from 'prop-types';
+import Forminput from '../components/Form/Forminput';
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   this.props.form.validateFields((err, values) => {
+  //     if (!err) {
+  //       console.log('Received values of form: ', values);
+  //     }
+  //   });
+  // };
 
   render() {
-    return <Errorboundary>lofin page</Errorboundary>;
+    return (
+      <div id="login">
+        <h1>Welcome Back!</h1>
+
+        <form action="/" method="post">
+          <Forminput
+            label="Email Address"
+            inputProps={{
+              type: 'email',
+              required: true,
+              autoComplete: 'off'
+            }}
+          />
+          <Forminput
+            label="Password"
+            inputProps={{
+              type: 'password',
+              required: true,
+              autoComplete: 'off'
+            }}
+          />
+
+          <p className="forgot">
+            <a href="#">Forgot Password?</a>
+          </p>
+
+          <button className="button button-block">Log In</button>
+        </form>
+      </div>
+    );
   }
 }
-NormalLoginForm.propTypes = {
-  form: PropTypes.object
+Login.propTypes = {
+  // form: PropTypes.object
 };
 
-export default NormalLoginForm;
+export default Login;
