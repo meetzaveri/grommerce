c4696bead4d0e9df71293afdbf5dce7d8c15266f
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-10-31 01:28:54 +0530

    Main component login replace with loginsignup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss';
-import Login from './components/login';
+import LoginSignup from './components/LoginSignup/LoginSignup';
 import Routes from './config/routes';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 class Main extends React.Component {
   render() {
     return (
       <Router>
-        <Route path={Routes.login} component={Login} />
+        <Route path={Routes.login} component={LoginSignup} />
       </Router>
     );
   }
