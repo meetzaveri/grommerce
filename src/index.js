597e7177c9c23b6ba32493dc35c6fea638ac801f
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-21 21:57:21 +0530

    Made changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss';
 import LoginSignup from './components/LoginSignup/LoginSignup';
-import Routes from './config/routes';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 class Main extends React.Component {
   render() {
     return (
