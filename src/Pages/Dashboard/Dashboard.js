d5796fba51373fe1023e4ce11759ca1022aa6b43
Prateek Gogia <gogia.prateek@hotmail.com>
2019-06-18 22:36:06 +0530

    :confused: Added react-siema carousel

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import {Row} from 'reactstrap'
+import { Row, Card, CardBody } from 'reactstrap';
+import { Colxx } from '../../components/CustomBootstrap';
+import ReactSiema from 'react-siema';
 
 @inject('headerStore')
 @observer
@@ -20,9 +22,60 @@ export default class Dashboard extends Component {
   render() {
     const { headerStore } = this.props;
     return (
-      <Row>
-        
-      </Row>
+      <>
+        <Row>
+          <Colxx lg="12" xl="6">
+            <div className="icon-cards-row">
+              <ReactSiema>
+                <div className="icon-row-item">
+                  <Card>
+                    <CardBody className="text-center">
+                      <i className="iconsminds-clock" />
+                      <p className="card-text font-weight-semibold mb-0">
+                        Pending Orders
+                      </p>
+                      <p className="lead text-center">14</p>
+                    </CardBody>
+                  </Card>
+                </div>
+                <div className="icon-row-item">
+                  <Card>
+                    <CardBody className="text-center">
+                      <i className="iconsminds-basket-coins" />
+                      <p className="card-text font-weight-semibold mb-0">
+                        Completed Orders
+                      </p>
+                      <p className="lead text-center">32</p>
+                    </CardBody>
+                  </Card>
+                </div>
+                <div className="icon-row-item">
+                  <Card>
+                    <CardBody className="text-center">
+                      <i className="iconsminds-arrow-refresh" />
+                      <p className="card-text font-weight-semibold mb-0">
+                        Refund Requests
+                      </p>
+                      <p className="lead text-center">14</p>
+                    </CardBody>
+                  </Card>
+                </div>
+                <div className="icon-row-item">
+                  <Card>
+                    <CardBody className="text-center">
+                      <i className="iconsminds-mail-read" />
+                      <p className="card-text font-weight-semibold mb-0">
+                        New Comments
+                      </p>
+                      <p className="lead text-center">25</p>
+                    </CardBody>
+                  </Card>
+                </div>
+              </ReactSiema>
+            </div>
+          </Colxx>
+        </Row>
+      </>
     );
   }
 }
