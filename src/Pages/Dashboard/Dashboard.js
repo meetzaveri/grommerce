0b640fd64a0f62334916aa813fc28ed177c7554d
Prateek Gogia <gogia.prateek@hotmail.com>
2019-06-22 00:17:30 +0530

    Fixed styling of Recent Orders

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Row, Card, CardBody, NavLink, Badge } from 'reactstrap';
+import { Row, Card, CardBody, CardTitle, NavLink, Badge } from 'reactstrap';
 import { Colxx } from '../../components/CustomBootstrap';
 import ReactSiema from '../../components/ReactSiema/ReactSiemaCarousel';
 import { LineShadow } from '../../components/Charts';
 import { lineChartConfig } from 'config/chartConfig';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import productsData from 'Data/products.json';
+import images from '../../../assets/img/*.jpg';
 
 const recentOrders = productsData.data.slice(0, 6);
 
@@ -92,57 +93,63 @@ export default class Dashboard extends Component {
               <LineShadow {...lineChartConfig} />
             </div>
           </Colxx>
-          <Colxx lg="12" xl="6">
+          <Colxx lg="12" xl="6" className="mb-4">
             <Card>
               <div className="position-absolute card-top-buttons">
                 <button className="btn btn-header-light icon-button">
                   <i className="simple-icon-ref" />
                 </button>
               </div>
-              <div className="scroll dashboard-list-with-thumbs">
-                <PerfectScrollbar
-                  option={{ suppressScrollX: true, wheelPropagation: false }}
-                >
-                  {recentOrders.map((order, index) => {
-                    return (
-                      <div key={index} className="d-flex flex-row mb-3">
-                        <NavLink
-                          to="/app/pages/details"
-                          className="d-block position-relative"
-                        >
-                          <img
-                            src={order.img}
-                            alt={order.name}
-                            className="list-thumbnail border-0"
-                          />
-                          <Badge
-                            key={index}
-                            className="position-absolute badge-top-right"
-                            color={order.statusColor}
-                            pill
+              <CardBody>
+                <CardTitle>
+                  {' '}
+                  <h5 className="card-title">Recent Orders</h5>{' '}
+                </CardTitle>
+                <div className="scroll dashboard-list-with-thumbs">
+                  <PerfectScrollbar
+                    option={{ suppressScrollX: true, wheelPropagation: false }}
+                  >
+                    {recentOrders.map((order, index) => {
+                      return (
+                        <div key={index} className="d-flex flex-row mb-3">
+                          <NavLink
+                            to="/app/pages/details"
+                            className="d-block position-relative"
                           >
-                            {order.status}
-                          </Badge>
-                        </NavLink>
-
-                        <div className="pl-3 pt-2 pr-2 pb-2">
-                          <NavLink to="/app/pages/details">
-                            <p className="list-item-heading">{order.name}</p>
-                            <div className="pr-4">
-                              <p className="text-muted mb-1 text-small">
-                                {order.descrition}
-                              </p>
-                            </div>
-                            <div className="text-primary text-small font-weight-medium d-none d-sm-block">
-                              {order.createDate}
-                            </div>
+                            <img
+                              src={order.img}
+                              alt={order.name}
+                              className="list-thumbnail border-0"
+                            />
+                            <Badge
+                              key={index}
+                              className="position-absolute badge-top-right"
+                              color={order.statusColor}
+                              pill
+                            >
+                              {order.status}
+                            </Badge>
                           </NavLink>
+
+                          <div className="pl-3 pt-2 pr-2 pb-2">
+                            <NavLink to="/app/pages/details">
+                              <p className="list-item-heading">{order.name}</p>
+                              <div className="pr-4">
+                                <p className="text-muted mb-1 text-small">
+                                  {order.descrition}
+                                </p>
+                              </div>
+                              <div className="text-primary text-small font-weight-medium d-none d-sm-block">
+                                {order.createDate}
+                              </div>
+                            </NavLink>
+                          </div>
                         </div>
-                      </div>
-                    );
-                  })}
-                </PerfectScrollbar>
-              </div>
+                      );
+                    })}
+                  </PerfectScrollbar>
+                </div>
+              </CardBody>
             </Card>
           </Colxx>
         </Row>
