88e2a18e6c7041adb5e3bfd67255c86ed559a0f1
Prateek Gogia <gogia.prateek@hotmail.com>
2019-08-21 11:11:24 +0530

    :gift: Added BreadCrumb Component

--- src/Pages/Product/ProductInfo.js
+++ src/Pages/Product/ProductInfo.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import Breadcrumb from '../../components/Breadcrumb';
+
+const BreadcrumbItems = [
+  { name: 'Home', link: '/' },
+  { name: 'Pages', link: '/pages' },
+  { name: 'Product', link: '/product' }
+];
 
 class ProductInfo extends Component {
   render() {
@@ -7,6 +14,9 @@ class ProductInfo extends Component {
         <div className="row">
           <div className="col-12">
             <h1>Chocolate Cake</h1>
+            <nav className="pt-0 breadcrumb-container d-none d-sm-block d-lg-inline-block">
+              <Breadcrumb items={BreadcrumbItems} />
+            </nav>
           </div>
         </div>
       </div>
