9b452cb8dd6699502631071a17102ed2bac5ea5c
Prateek Gogia <gogia.prateek@hotmail.com>
2019-11-21 23:20:53 +0530

    Added onchange and values in input

--- src/components/Modals/AddProductModal.js
+++ src/components/Modals/AddProductModal.js
@@ -13,6 +13,8 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import gql from 'graphql-tag';
 
+import { useMutation } from '@apollo/react-hooks';
+
 const validationSchema = Yup.object().shape({
   name: Yup.string().required(),
   imageUrl: Yup.string().required(),
@@ -31,62 +33,74 @@ const validationSchema = Yup.object().shape({
 
 const ADD_PRODUCT = gql`
   mutation createProduct(
-    $id: UUID, 
-    name: String!, 
-    companyId: UUID!, 
-    quantity: Int!, 
-    price: Float, 
-    marketPrice: Float!, 
-    discountedPrice: Float, 
-    owner: UUID!, 
-    imageUrl: String, 
-    productDescription: String, 
-    manufactureDate: Date, 
-    expireDate: Date, 
-    variants: String) {
+    $id: UUID
+    $name: String!
+    $companyId: UUID!
+    $quantity: Int!
+    $price: Float
+    $marketPrice: Float!
+    $discountedPrice: Float
+    $owner: UUID!
+    $imageUrl: String
+    $productDescription: String
+    $manufactureDate: Date
+    $expireDate: Date
+    $variants: String
+  ) {
     createProduct(
       input: {
         product: {
-          id: $id, 
-          name: $name, 
-          companyId: $companyId, 
-          quantity: $quantity, 
-          price: $price, 
-          marketPrice: $marketPrice, 
-          discountedPrice: $discountedPrice, 
-          owner: $owner, 
-          imageUrl: $imageUrl, 
-          productDescription: $productDescription, 
-          manufactureDate: $manufactureDate, 
-          expireDate: $expireDate, 
-          variants: $variants}}) {
-            product {
-              name
-            }
+          id: $id
+          name: $name
+          companyId: $companyId
+          quantity: $quantity
+          price: $price
+          marketPrice: $marketPrice
+          discountedPrice: $discountedPrice
+          owner: $owner
+          imageUrl: $imageUrl
+          productDescription: $productDescription
+          manufactureDate: $manufactureDate
+          expireDate: $expireDate
+          variants: $variants
+        }
+      }
+    ) {
+      product {
+        name
+      }
     }
   }
 `;
 
-const AddProductModal = ({ isOpen, toggle }) => {
+function AddProductModal({ isOpen, toggle }) {
+  const [addProduct, { data }] = useMutation(ADD_PRODUCT);
+
   return (
     <Modal isOpen={isOpen} toggle={toggle} wrapClassName="modal-right">
       <ModalHeader className="d-flex align-items-center py-0" toggle={toggle}>
         <h3>Add a Product</h3>
       </ModalHeader>
       <ModalBody>
-        <Formik
-          validationSchema={validationSchema}
-          onSubmit={e => console.log('e', e)}
-        >
-          {() => {
+        <Formik validationSchema={validationSchema}>
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+          }) => {
             return (
-              <>
+              <form onSubmit={handleSubmit}>
                 <FormGroup>
                   <Label for="name">Name</Label>
                   <Input
                     type="text"
                     name="name"
                     placeholder="Enter Title of the Product"
+                    value={values.name}
                   />
                 </FormGroup>
                 <FormGroup>
@@ -95,6 +109,8 @@ const AddProductModal = ({ isOpen, toggle }) => {
                     name="imageUrl"
                     type="text"
                     placeholder="Enter Image URL of the Product"
+                    onChange={handleChange}
+                    value={values.imageUrl}
                   />
                 </FormGroup>
                 <FormGroup>
@@ -103,15 +119,27 @@ const AddProductModal = ({ isOpen, toggle }) => {
                     name="description"
                     type="textarea"
                     placeholder="Enter Description of the Product"
+                    onChange={handleChange}
+                    value={values.description}
                   />
                 </FormGroup>
                 <FormGroup>
                   <Label for="manufactureDate">Manufacture Date</Label>
-                  <Input name="manufactureDate" type="date" />
+                  <Input
+                    name="manufactureDate"
+                    type="date"
+                    onChange={handleChange}
+                    value={values.manufactureDate}
+                  />
                 </FormGroup>
                 <FormGroup>
                   <Label for="expireDate">Expire Date</Label>
-                  <Input name="expireDate" type="date" />
+                  <Input
+                    name="expireDate"
+                    type="date"
+                    onChange={handleChange}
+                    value={values.expireDate}
+                  />
                 </FormGroup>
                 <FormGroup>
                   <Label for="mrp">Maximum Retail Price (MRP)</Label>
@@ -120,6 +148,8 @@ const AddProductModal = ({ isOpen, toggle }) => {
                     type="number"
                     placeholder="Enter MRP of Product"
                     min="0"
+                    onChange={handleChange}
+                    value={values.mrp}
                   />
                 </FormGroup>
                 <FormGroup>
@@ -129,11 +159,18 @@ const AddProductModal = ({ isOpen, toggle }) => {
                     type="number"
                     placeholder="Enter Discounted Price of Product"
                     min="0"
+                    onChange={handleChange}
+                    value={values.discountPrice}
                   />
                 </FormGroup>
                 <FormGroup>
                   <Label for="variants">Variants</Label>
-                  <Input name="variants" type="select" placeholder="">
+                  <Input
+                    name="variants"
+                    type="select"
+                    onChange={handleChange}
+                    value={values.variants}
+                  >
                     <option value="" disabled className="text-muted">
                       Enter Discounted Price of Product
                     </option>
@@ -153,9 +190,11 @@ const AddProductModal = ({ isOpen, toggle }) => {
                     type="number"
                     placeholder="Enter Quantity of Product in numbers"
                     min="0"
+                    onChange={handleChange}
+                    value={values.quantity}
                   />
                 </FormGroup>
-              </>
+              </form>
             );
           }}
         </Formik>
@@ -168,7 +207,7 @@ const AddProductModal = ({ isOpen, toggle }) => {
       </ModalFooter>
     </Modal>
   );
-};
+}
 
 AddProductModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
